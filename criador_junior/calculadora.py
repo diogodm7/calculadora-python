bf46073f473ea345a3241b6f692ae17bb6db6c19
Diogo DAS <diogominion7@gmail.com>
2024-04-30 21:12:49 -0300

    coloquei a função subtração

--- criador_junior/calculadora.py
+++ criador_junior/calculadora.py
@@ -8,6 +8,9 @@ def multiplicacao(valor1, valor2):
 
 def divisao(valor1, valor2):
     return valor1 / valor2
+
+def subtracao(valor3, valor4):
+    return valor3 - valor4
 #parte de ação 
 
 while True:
@@ -15,10 +18,22 @@ while True:
     resultado = input("para somar digite 1, multiplicar 2, dividir 3: ")
 
 
-    valor1 = float(input("digite o primeiro valor: "))
+    valor1 = float(input("diga a primeira parcela: "))
     
-    valor2 = float(input("digite o segundo valor: "))
-     
+    valor2 = float(input("diga a segunda parcela: "))
+    
+    valor3 =  float(input("diga o minuendo: "))
+
+    valor4 = float(input("diga o subtraendo: "))
+
+    valor5 = float(input("diga o multiplicado: "))
+
+    valor6 = float(input("diga o multiplicador"))
+
+    valor7 = float(input("diga o dividendo: "))
+
+    valor8 = float(input("diga o divisor: "))
+    
     if resultado == "2":
         
         resposta2 = multiplicacao(valor1, valor2)
@@ -32,4 +47,4 @@ while True:
         resposta = funcsoma(valor1, valor2)
         print(valor1, "+", valor2, "=", resposta)
 
-    print("uso da git")
+    
