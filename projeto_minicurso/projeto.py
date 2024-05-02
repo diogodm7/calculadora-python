49eda326bdac01f468533915b370175389543676
Diogo DAS <diogominion7@gmail.com>
2024-05-02 18:46:21 -0300

    comando import ativo

---
+++ projeto_minicurso/projeto.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+#importar base de dados 
+
+tabela_vendas = pd.read_excel("C:/Users/aspdiogo/Documents/workspace/projeto_minicurso/vendas.xlsx")
+
+print(tabela_vendas)
+
+#importar base de dados 
+
+#visualizar base de dados 
+
+#faturamento por loja 
+
+#quantidade de produtos por loja
+
+#ticket médio por produto em cada loja
+
+#enviar email com relatório
